762cd8afb2ddca9287da1161f478dcaf1bbf4796
Garrett Smith <19smitgr@users.noreply.github.com>
2018-05-12 12:56:56 -0400

    Update files for final lesson plan

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ let inputBox = document.getElementById('todoInput');
 inputBox.addEventListener('keydown', display);
 
 function display(e) {
+
+    // 13 is enter key
     if (e.keyCode == 13) {
         let div = document.createElement('div');
         div.className += 'todoItem';
         div.innerHTML = inputBox.value;
         document.getElementById('todos').appendChild(div);
+
+        // let them discover this comand by themselves
+        inputBox.value = "";
     }
+
 }
